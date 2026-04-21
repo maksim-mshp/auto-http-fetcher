b3597d31935a181d6b291a1e46c105cbd1c45ff4
kinzool <solovyevkirill2007@mail.ru>
2026-04-21 19:39:58 +0300

    fix: поменял context.Background на r.Context

--- internal/analytics/infra/http/get.go
+++ internal/analytics/infra/http/get.go
@@ -1,13 +1,12 @@
 package http
 
 import (
-	"context"
 	"encoding/json"
 	"net/http"
 )
 
 func (h *Handler) HandleGet(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
+	ctx := r.Context()
 	analytics, err := h.analytics.Get(ctx)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
